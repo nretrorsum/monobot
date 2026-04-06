d618cd3cc48528d6e2f43c4a1fd9f65710332f09
nretrorsum <daniel0629692@gmail.com>
2026-04-06 23:50:38 +0300

    Fix migration que

--- alembic/versions/e5abe24977cc_update_income_date_field.py
+++ alembic/versions/e5abe24977cc_update_income_date_field.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'e5abe24977cc'
-down_revision: Union[str, None] = '8bdaed46bf4d'
+down_revision: Union[str, None] = 'e35551190d22'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
